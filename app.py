81c34fc56a530dc5fffadafb508d1767fcb62aaf
Steven Jordan <pydtyper@gmail.com>
2020-09-20 11:01:28 -0400

    updated files

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 # Import your Game class
-
+from phrasehunter.game import Game
 # Create your Dunder Main statement.
-
+game = Game()
 # Inside Dunder Main:
 ## Create an instance of your Game class
 ## Start your game by calling the instance method that starts the game loop
+
+
+if __name__ == "__main__":
+    game.start()
--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,11 +1,12 @@
 import random
+from .phrase import Phrase
 
 class Game:
 
     def __init__(self):
 
         self.missed = 0
-        self.phrases = []
+        self.phrases = [Phrase("cat in the hat"), Phrase("boy who cried wolf"), Phrase("the fox and the hound"), Phrase("the sword in the stone"), Phrase("robin hood")]
         self.active_phrase = None
         self.guesses = []
 
@@ -15,14 +16,19 @@ class Game:
         self.welcome()
 
         # creates game loop
-
+        self.active_phrase = self.get_random_phrase()
         # calls get_guess()
-
+        guess = self.get_guess()
         # adds player's guess to the guesses list
-
+        self.guesses.append(guess)
         # increments missed if guess incorrect
-
+        if guess not in self.active_phrase:
+            self.missed += 1
+            print(f"{guess} isn't in the phrase!")
+            print(f"You have {5 - self.missed} lives left!")
         # calls game_over()
+        if self.missed > 5:
+            game_over()
 
 
     def get_random_phrase(self):
@@ -37,11 +43,19 @@ class Game:
     
     def get_guess(self):
 
-        # get guess from user; adds it to self.guesses
-        pass
+        # get guess from user, verifies it's a letter and hasn't been guessed yet
+        guess = ""
+        while not guess.isalpha():
+            guess = input("Enter a letter to guess >> ").lower()
 
+            if guess in self.guesses:
+                print("You've alread guessed that letter!")
+                print(self.guesses)
+                guess = ""
+
+        return guess
 
     def game_over(self):
 
         # print win or lose message
-        pass
+        print("Game Over!")
