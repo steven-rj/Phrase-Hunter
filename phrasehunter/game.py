6d787959f07f9e1b6d324aed60282cad95951b55
Steven Jordan <pydtyper@gmail.com>
2020-09-22 10:49:08 -0400

    completed game loop

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -17,25 +17,27 @@ class Game:
 
         self.welcome()
         self.active_phrase = self.get_random_phrase()
-        print(self.active_phrase)
         # creates game loop
-        while True:            
+        while True:       
+            print(f"\n\nCategory: {self.active_phrase}\n")
             self.active_phrase.display(self.guesses)
             
             # calls get_guess()
             guess = self.get_guess()
             # adds player's guess to the guesses list
             self.guesses.append(guess)
-            print(self.guesses)
+            # print(self.guesses)
             self.active_phrase.check_letter(guess)
             # increments missed if guess incorrect
             if not self.active_phrase.check_letter(guess):
                 self.missed += 1
-                print(f"{guess} isn't in the phrase!")
-                print(f"You have {5 - self.missed} lives left!")
+                print(f"\n** '{guess}' isn't in the phrase!")
+                print(f"** {5 - self.missed} chances left!")
             # calls game_over()
-            if self.active_phrase.check_complete(self.guesses):
-                self.game_over()
+            if self.missed > 4:
+                self.game_over("lose")
+            elif self.active_phrase.check_complete(self.guesses):
+                self.game_over("win")
 
 
     def get_random_phrase(self):
@@ -53,23 +55,28 @@ class Game:
         # get guess from user, verifies it's a letter and hasn't been guessed yet
         guess = ""
         while not guess.isalpha():
-            guess = input("Enter a letter to guess >> ").lower()
+            guess = input("\nEnter a letter to guess >> ").lower()
 
             if guess in self.guesses:
-                print("You've alread guessed that letter!")
+                print("\nYou've alread guessed that letter!")
                 print(f"Past Guesses: {self.guesses}")
                 guess = ""
 
         return guess
 
-    def game_over(self):
+    def game_over(self, message):
 
         # print win or lose message
-        print("Game Over!")
-
+        if message == "lose":
+            print("\nGAME OVER!!")
+            print(f"The phrase was: '{self.active_phrase}''")
+        elif message == "win":
+            print("\nYOU WON!!")
+        
+        # ask for replay
         replay = ""
         while replay != "y" and replay != "n":
-            replay = input("Play again? [y/n] >> ").lower()
+            replay = input("\nPlay again? [y/n] >> ").lower()
         
         if replay == "y":
             game = Game()
