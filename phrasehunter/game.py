83394ff76f4b852d458191b927bd459e4ac4e60f
pyd_typer <pydtyper@gmail.com>
2020-11-29 19:33:00 -0500

    added phrases

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -42,7 +42,8 @@ class Game:
         Phrase("Snow White and the Seven Dwarves"), Phrase("Tom and Huck"), Phrase("White Fang"), Phrase("Toy Story"),
         Phrase("A Goofy Movie"), Phrase("James and the Giant Peach"), Phrase("Treasure Planet"),
         Phrase("The Reluctant Dragon"), Phrase("Star Wars Rogue One"), Phrase("The Phantom Menace"),
-        Phrase("Attack of the Clones"), Phrase("Revenge of the Sith"), Phrase("Big Hero Six")]},
+        Phrase("Attack of the Clones"), Phrase("Revenge of the Sith"), Phrase("A New Hope"),
+        Phrase("The Empire Strikes Back"), Phrase("Return of the Jedi"), Phrase("Big Hero Six")]},
         
         {"Disney Characters": [Phrase("Mulan"), Phrase("Shan Yu"), Phrase("Simba"), Phrase("Scar"),
         Phrase("Ariel"), Phrase("Ursula"), Phrase("Aladdin"), Phrase("Jafar"), Phrase("Mr Incredible"), 
