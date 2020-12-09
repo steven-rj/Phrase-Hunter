a13b67171ced63500a18fef892a89258a263375c
pyd_typer <pydtyper@gmail.com>
2020-12-08 22:50:32 -0500

    added phrases

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -56,7 +56,7 @@ class Game:
         Phrase("Captain Salazar"), Phrase("Davy Jones"), Phrase("Sheriff Woody"), Phrase("Buzz Lightyear"),
         Phrase("Goofy"), Phrase("Max"),Phrase("Esmerelda"), Phrase("Tarzan"), Phrase("Queen of Hearts"), Phrase("Happy"),
         Phrase("Doppy"), Phrase("Doc"), Phrase("Mando"), Phrase("Grogu"), Phrase("Ahsoka Tano"), Phrase("Luke Skywalker"),
-        Phrase("Anakin Skywalker"), Phrase("Boba Fett"), Phrase("Obi Wan Kenobi")]} ]
+        Phrase("Anakin Skywalker"), Phrase("Boba Fett"), Phrase("Obi Wan Kenobi"), Phrase("General Grievous")]} ]
         self.active_phrase = None
         self.guesses = []
         self.category = ""
